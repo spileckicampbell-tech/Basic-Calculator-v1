bb1b5ae2eb121148b726977df2093d3659c8e885
spileckicampbell-tech <spileckicampbell@gmail.com>
2026-01-03 23:11:30 -0500

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,19 @@
+// CALCULATOR PROGRAM BY SEBASTIAN PILECKI-CAMPBELL
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
